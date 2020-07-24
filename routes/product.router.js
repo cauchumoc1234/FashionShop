99c9d1faf03f1cde34a6519ffe84add8203c5aec
cauchumoc1234 <ducanhfo3888@gmail.com>
2020-07-24 18:55:03 +0700

    Next commit

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,12 +1,6 @@
 const express = require('express')
 const product_route = express.Router()
-product_route.get('/',(req,res)=>{
-    res.render('product')
-})
-product_route.get('/say',(req,res)=>{
-    res.render('home')
-})
-product_route.get('/category',(req,res)=>{
-    res.render('product')
-})
+const product_controller = require('../controllers/product.controller')
+product_route.get('/',product_controller.show)
+product_route.get('/category',product_controller.show)
 module.exports = product_route;
