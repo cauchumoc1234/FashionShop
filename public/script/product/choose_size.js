a59c0cd562982c7de2083f70e95da15445b6fb50
cauchumoc1234 <ducanhfo3888@gmail.com>
2020-09-19 22:23:00 +0700

    choose size ok

--- public/script/product/choose_size.js
+++ public/script/product/choose_size.js
@@ -3,10 +3,7 @@ async function addEvent(){
     var sizeBtn = await document.getElementsByClassName('size-btn')
      for(var i = 0 ; i < sizeBtn.length;i++){    
         sizeBtn[i].addEventListener('click',function(){
-            this.getElementsByClassName('choice').value = "checked";
-            document.getElementById('amount').max = this.value
+            document.getElementById('amount').max = this.getElementsByClassName('instock')[0].getAttribute('value');
         })
-        }
     }
-
-
+}
