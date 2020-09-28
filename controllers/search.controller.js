a91a1ea858a708f93da8d100ffff59b31a15990d
cauchumoc1234 <ducanhfo3888@gmail.com>
2020-09-28 22:19:40 +0700

    Change some link,add cart button

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -5,11 +5,36 @@ module.exports.find = async function(req,res){
     var page = parseInt(req.query.page) || 1;
     var shoe_rs = await Shoes.find({$or: [{productname:{$regex:req.query.q,$options:"$i"}},{type:{$regex:req.query.q,$options:"$i"}},{brand:{$regex:req.query.q,$options:["$i","$x"]}}]}).exec()
     var cloth_rs = await Clothes.find({$or: [{productname:{$regex:req.query.q,$options:"$i"}},{type:{$regex:req.query.q,$options:"$i",$options:"$x"}}]}).exec()
-    var total = shoe_rs.concat(cloth_rs).sort(function(a,b){
-        if(a.updated > b.updated) return -1;
-        else return 1;
-    });
-    var result = total.slice((page-1)*12,page*12);
+    var total = shoe_rs.concat(cloth_rs);
+    switch(req.query.order){
+        case "date-desc":
+            total.sort(function(a,b){
+                if(a.updated > b.updated) return -1;
+                else return 1;
+            })
+            break;
+        case "date-asc":
+            total.sort(function(a,b){
+                if(a.updated < b.updated) return -1;
+                else return 1;
+            })
+            break;
+        case "price-asc":
+            total.sort(function(a,b){
+                if(a.price < b.price) return -1;
+                else return 1;
+            })
+            break;
+        case "price-desc":
+            total.sort(function(a,b){
+                if(a.price > b.price) return -1;
+                else return 1;
+            })
+            break;
+        default:
+            break;
+    }
+    var result = total.slice((page-1)*15,page*15);
     var max_page = Math.ceil(total.length/12);
     res.render('search/searched',{find:req.query.q,products:result,url:req.originalUrl,max_page : max_page,current_page : page})
 }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,14 @@
+const { JSONCookies } = require('cookie-parser')
 var User = require('../models/user.model')
 
 module.exports.show = function(req, res) {
-    // res.render('user/user_login')
     res.send("user")
 }
 module.exports.create = function(req, res) {
     res.render('user/user_create')
 }
 module.exports.postCreate = function(req, res) {
+    //Cần thêm kiểm tra user đã tồn tại chưa
     User.create({
         username: req.body.username,
         password: req.body.password,
@@ -26,20 +27,23 @@ module.exports.login = function(req, res) {
 module.exports.logout = function(req, res) {
     document.cookie = "_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
 }
-module.exports.postLogin = function(req, res) {
-    User.find({ 'username': req.body.username, 'password': req.body.password }, 'username password', function(err, users) {
-        if (err) return handleError(err);
-        res.cookie('id', users[0]._id, { signed: true })
-        res.redirect('/')
-    })
+module.exports.postLogin = async function(req, res) {
+    var user = await User.find({ 'username': req.body.username, 'password': req.body.password }).exec();
+        if(user.length >0){
+            res.cookie('id',user[0]._id,{signed:true})
+            res.redirect('/')
+        }
+        else{
+            res.render('user/user_login',{error: "Login info was wrong! Please submit the right one."})
+        }
 }
 module.exports.showProfile = async function(req, res) {
-    var id = req.signedCookies
-    var user = await User.find({"_id": req.signedCookies['id']},'username fullname').exec();
-    res.send(user)
-    res.render('user/profile')
+    var id = req.signedCookies.id
+    var user = await User.find({"_id": id},'username fullname').exec();
+    res.render('user/profile',{user: user})
 }
 module.exports.logout = function(req, res) {
     res.clearCookie('id');
     res.redirect('/')
 }
+//Làm page profile của user
